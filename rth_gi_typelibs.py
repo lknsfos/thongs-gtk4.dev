1ed72cd48bbf64ddf89de716de621a1f788cdee7
mikhael-qb <mikhael@qbsystems.biz>
2025-11-24 17:59:12 +0200

    fix hook

--- rth_gi_typelibs.py
+++ rth_gi_typelibs.py
@@ -1,8 +1,14 @@
 # rth_gi_typelibs.py
 import os
 import sys
+import subprocess
 
-# добавляем путь с typelib в GI_TYPELIB_PATH
-meipass = getattr(sys, '_MEIPASS', os.path.abspath('.'))
-typelib_path = os.path.join(meipass, 'gi', 'typelib')
-os.environ['GI_TYPELIB_PATH'] = typelib_path + ':' + os.environ.get('GI_TYPELIB_PATH', '')
+# НАДЕЖНЫЙ СПОСОБ: Находим системный путь к typelib
+try:
+    # Этот путь является стандартным для большинства дистрибутивов Linux
+    system_typelib_path = '/usr/lib/x86_64-linux-gnu/girepository-1.0'
+    if os.path.isdir(system_typelib_path):
+        os.environ['GI_TYPELIB_PATH'] = system_typelib_path
+except Exception as e:
+    # На случай, если что-то пойдет не так
+    print(f"FATAL: Could not set GI_TYPELIB_PATH. Error: {e}", file=sys.stderr)
