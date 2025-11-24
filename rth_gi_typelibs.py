8b2e787859db2dc7a516cd81ce6f08c970d16f0d
mikhael-qb <mikhael@qbsystems.biz>
2025-11-24 18:01:38 +0200

    fix hook2

--- rth_gi_typelibs.py
+++ rth_gi_typelibs.py
@@ -1,14 +1,29 @@
 # rth_gi_typelibs.py
 import os
 import sys
-import subprocess
 
-# НАДЕЖНЫЙ СПОСОБ: Находим системный путь к typelib
-try:
-    # Этот путь является стандартным для большинства дистрибутивов Linux
-    system_typelib_path = '/usr/lib/x86_64-linux-gnu/girepository-1.0'
-    if os.path.isdir(system_typelib_path):
-        os.environ['GI_TYPELIB_PATH'] = system_typelib_path
-except Exception as e:
-    # На случай, если что-то пойдет не так
-    print(f"FATAL: Could not set GI_TYPELIB_PATH. Error: {e}", file=sys.stderr)
+# Этот хук выполняется до запуска основного скрипта.
+# Его задача - установить переменные окружения, чтобы PyGObject нашел свои библиотеки.
+
+# 1. Путь к typelib файлам, которые PyInstaller упаковал.
+bundled_typelib_path = os.path.join(sys._MEIPASS, 'gi', 'typelib')
+
+# 2. Стандартные системные пути, где могут лежать typelib файлы.
+system_typelib_paths = [
+    '/usr/lib/x86_64-linux-gnu/girepository-1.0',
+    '/usr/lib/girepository-1.0',
+    '/usr/local/lib/girepository-1.0',
+]
+
+# 3. Формируем новый GI_TYPELIB_PATH.
+# Сначала добавляем упакованный путь, затем все существующие системные пути.
+new_path = [bundled_typelib_path]
+for path in system_typelib_paths:
+    if os.path.isdir(path):
+        new_path.append(path)
+
+# 4. Устанавливаем переменную окружения.
+os.environ['GI_TYPELIB_PATH'] = os.pathsep.join(new_path)
+
+# Для отладки: выводим установленный путь в консоль при запуске.
+print(f"DEBUG: GI_TYPELIB_PATH set to: {os.environ['GI_TYPELIB_PATH']}", file=sys.stderr)
